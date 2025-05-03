6a686b4313e0f2a98e39ca85684f37557132eb46
sachin kunwar <sachinkunwar284@gmail.com>
2025-05-03 15:29:22 +0545

    crud api

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express") // express require gareko
+const bookRoute = require("./routes/bookRoute")
+
+const app = express() // express lai trigger gareko 
+require("./database/connection")
+app.use(express.json())
+// let app = require("express")()
+
+// localhost:4000/api+ /hello = localhost:4000/api/hello
+// localhost:4000/api + /books/:id = localhost:4000/api/books/:id
+// localhost:4000/api/haha/ + /books = localhost:4000/api/haha//books
+app.use("/api/books",bookRoute)
+
+
+app.listen(4000, function () {
+    console.log("project/server/backend has started at port 4000")
+})
---
+++ database/connection.js
@@ -0,0 +1,33 @@
+
+const {Sequelize,DataTypes} =  require("sequelize")
+
+// const bookModel = require("./models/book.model")
+
+// const sequelize = require("sequelize")
+// const Sequelize = sequelize.Sequelize
+// const DataTypes = sequelize.DataTypes
+
+const sequelize = new Sequelize("postgresql://postgres.zohqjdrsruxnqrhgmkdq:Sass Development By DP@aws-0-ap-south-1.pooler.supabase.com:6543/postgres")
+
+sequelize.authenticate()
+.then(()=>{
+    console.log("Authenticated vayo, connected vayo !!")
+})
+.catch((err)=>{
+    console.log("Error aayo" +err)
+})
+
+const db  = {}
+db.Sequelize = Sequelize 
+db.sequelize = sequelize
+
+db.books =  require("./models/book.model")(sequelize,DataTypes)
+db.users = require("./models/user.model")(sequelize,DataTypes)
+db.products = require("./models/product.model")(sequelize,DataTypes)
+
+// migrate code ho yo chai hai 
+sequelize.sync({alter : false}).then(()=>{
+    console.log("Migrate vayo hai tw 🚀")
+})
+
+module.exports = db
