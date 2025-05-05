f36e057fd42e7d92a47643f38ac49d088e899563
sachin kunwar <sachinkunwar284@gmail.com>
2025-05-05 21:24:04 +0545

    delete and update operation

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -15,10 +15,10 @@ exports.addBook = async function(req,res){
     // const bookName = req.body.bookName
     // const bookPrice = req.body.bookPrice
 
-    const {bookNamess,bookPrice,bookAuthor,bookGenre} = req.body
+    const {bookName,bookPrice,bookAuthor,bookGenre} = req.body
     // check if all data aako xa vane only proceed , else not proceed throw error in response
     await books.create({
-        bookName : bookNamess,
+        bookName : bookName,
         price : bookPrice, 
         bookAuthor, 
         bookGenre
@@ -29,17 +29,43 @@ exports.addBook = async function(req,res){
     })
 }
 
-exports.deleteBook = function(req,res){
-    // logic to delete book
+exports.deleteBook = async function(req,res){
+    // first ma hami, kun book delete garna aatekoho tesko id lim. 
+    const id = req.params.id // const {id} = req.params
+//    const id = req.body.id 
+    // id payisakeypaxi, tyo id ko book chai books table bata udaidim 
+    await books.destroy({
+        where : {
+            id
+        }
+    }) // delete from books where id = id
     res.json({
       message : "Book Deleted successfully"
     })
 }
-exports.editBook = function(req,res){
-    // logic to update book
+exports.editBook = async function(req,res){
+   try {
+     // logic to update book
+    // kun id ko chai edit garne tyo id linu paryo . 
+    const id = req.params.id
+    // k k update garne tw .. 
+    const {bookName,price,bookAuthor,bookGenre} = req.body
+
+    await books.update({bookName,price, bookAuthor,bookGenre },{
+        where : {
+            id : id
+        }
+    })
+    // books.findByIdAndUpdate(id,{})
+
     res.json({
       message : "Book updated successfully"
     })
+   } catch (error) {
+    res.json({
+        message : "Something went wrong"
+    })
+   }
 }
 
 exports.singleFetchBook = async function(req,res){
